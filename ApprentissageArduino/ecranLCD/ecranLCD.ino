98704bbc71763c08edf0c1e9e2b9b913328086f5
Windowbow <windowbow.gaming@gmail.com>
2017-11-12 15:11:07 +0100

    Update ecranLCD.ino

--- ApprentissageArduino/ecranLCD/ecranLCD.ino
+++ ApprentissageArduino/ecranLCD/ecranLCD.ino
@@ -8,81 +8,39 @@ LiquidCrystal_I2C lcd(0x20,16,2);  // set the LCD address to 0x20 for a 16 chars
 
 void setup()
 {
+  //begin synchronise ordi et carte à une vitesse de 9600 baud
   Serial.begin(9600);
+  
   lcd.init();                      // initialize the lcd 
  
   // Print a message to the LCD.
+  //allume l'écran
   lcd.backlight();
-  lcd.print("FUCK YOU!");
+  
+  //place le curseur au début de l'écran
   lcd.home();
 }
 
 void loop()
 {
-lcd.print("FUCK YOU!");
-delay(500);
-lcd.home();
-lcd.print(" FUCK YOU!");
-delay(500);
-lcd.home();
-lcd.print("  FUCK YOU!");
-delay(500);
-lcd.home();
-lcd.print("   FUCK YOU!");
-delay(500);
-lcd.home();
-lcd.print("    FUCK YOU!");
-delay(500);
-lcd.home();
-lcd.print("     FUCK YOU!");
-delay(500);
-lcd.home();
-lcd.print("      FUCK YOU!");
-delay(500);
-lcd.home();
-lcd.print("       FUCK YOU!");
-delay(500);
-lcd.home();
-lcd.print("!       FUCK YOU");
-delay(500);
-lcd.home();
-lcd.print("U!       FUCK YO");
-delay(500);
-lcd.home();
-lcd.print("OU!       FUCK Y");
-delay(500);
-lcd.home();
-lcd.print("YOU!       FUCK ");
-delay(500);
-lcd.home();
-lcd.print(" YOU!       FUCK");
-delay(500);
-lcd.home();
-lcd.print("K YOU!       FUC");
-delay(500);
-lcd.home();
-lcd.print("CK YOU!       FU");
-delay(500);
-lcd.home();
-lcd.print("UCK YOU!       F");
-delay(500);
-lcd.home();
+  
   if (Serial.available() > 0) {
-                // read the incoming byte:
-                //incomingByte = Serial.read();
-                char texte;
-                texte=Serial.read();
-                if (texte=='0'){
-                  lcd.print("Bienvenue");
-                }
-                if (texte=='1'){
-                  lcd.print("Ok");
-                }
-                if (texte=='2'){
-                  lcd.print("Recommencer");
-                }
-                if (texte=='3'){
-                  lcd.print("Recommencer");
-                }
+      // read the incoming byte:
+      //incomingByte = Serial.read();
+      char texte;
+      texte=Serial.read();
+      if (texte=='0'){
+        lcd.clear();
+        lcd.print("Bienvenue");
+      }
+      if (texte=='1'){
+        lcd.clear();
+        lcd.print("Ok");
+      }
+      if (texte=='2'){
+        lcd.clear();
+        lcd.print("Recommencer");
+      }
   }
-  }
+  
+}
